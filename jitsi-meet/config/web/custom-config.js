dab86b8b3db462972b547d28e264e2d80b0d0671
TequieroD <stu87231@gmail.com>
2022-10-14 07:21:41 +0000

    add custom web config

---
+++ jitsi-meet/config/web/custom-config.js
@@ -0,0 +1,53 @@
+config.toolbarButtons= [
+    'camera',
+    'chat',
+    'closedcaptions',
+    'desktop',
+    'download',
+    'feedback',
+    'filmstrip',
+    'fullscreen',
+    'hangup',
+    'help',
+    'invite',
+    'microphone',
+    'participants-pane',
+    'profile',
+    'raisehand',
+    'select-background',
+    'settings',
+    'shortcuts',
+    'stats',
+    'tileview',
+    'toggle-camera',
+    'videoquality',
+    '__end'
+ ];
+config.hideConferenceSubject=true;
+config.videoQuality= {
+    disabledCodec: 'VP8',
+    preferredCodec: 'VP9',
+    enforcePreferredCodec: true,
+    maxBitratesVideo: {
+            H264: {
+                low: 200000,
+                standard: 500000,
+                high: 1500000
+            },
+            VP8 : {
+                low: 200000,
+                standard: 500000,
+                high: 1500000
+            },
+            VP9: {
+                low: 100000,
+                standard: 300000,
+                high: 1200000
+            }
+    },    
+    minHeightForQualityLvl: {
+        360: 'standard',
+        720: 'high'
+    },
+    resizeDesktopForPresenter: false
+};
---
+++ jitsi-meet/config/web/custom-interface_config.js
@@ -0,0 +1,3 @@
+interfaceConfig.JITSI_WATERMARK_LINK='{{ .Values.publicURL }}';
+interfaceConfig.LIVE_STREAMING_HELP_LINK='{{ .Values.publicURL }}';
+interfaceConfig.SUPPORT_URL='{{ .Values.publicURL }}';
